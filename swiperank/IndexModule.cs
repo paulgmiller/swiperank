d6743e1ba6d0a3b2bcda7fd657f85b2ae4203656
Paul Miller <paul.miller@gmail.com>
2015-10-04 21:59:44 -0700

    add nancy stuff

---
+++ swiperank/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace swiperank
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}

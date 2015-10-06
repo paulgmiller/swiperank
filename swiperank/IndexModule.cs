48d7389d3101c3d56f66a92260bd0d40eab37803
Paul Miller <paul.miller@gmail.com>
2015-10-05 23:03:23 -0700

    hash upload

--- swiperank/IndexModule.cs
+++ swiperank/IndexModule.cs
@@ -4,6 +4,7 @@
     using Microsoft.WindowsAzure.Storage.Blob;
     using Microsoft.WindowsAzure.Storage;
     using System.Security.Cryptography;
+    using System;
 
     public class IndexModule : NancyModule
     {
@@ -24,7 +25,8 @@
                 await rankings.CreateIfNotExistsAsync();
                 MD5 md5Hasher = MD5.Create();
                 var hash = md5Hasher.ComputeHash(this.Request.Body);
-                var blob = rankings.GetBlockBlobReference(param.list + "/" + hash.ToString());
+                var blob = rankings.GetBlockBlobReference(param.list + "/" + BitConverter.ToString(hash).ToString());
+
 
                 await blob.UploadFromStreamAsync(this.Request.Body);
                 return HttpStatusCode.OK;

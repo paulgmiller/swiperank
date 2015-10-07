3b321970a25c2f082a8c2543e82b07bc77689b45
Paul Miller <paul.miller@gmail.com>
2015-10-06 19:37:34 -0700

    seek to begining of stream for upload

--- swiperank/IndexModule.cs
+++ swiperank/IndexModule.cs
@@ -1,8 +1,8 @@
 ﻿namespace swiperank
 {
     using Nancy;
-    using Microsoft.WindowsAzure.Storage.Blob;
     using Microsoft.WindowsAzure.Storage;
+    using Microsoft.Azure;
     using System.Security.Cryptography;
     using System;
 
@@ -19,15 +19,15 @@
             Post["/ranking/{list}", runAsync: true] = async (param, token) =>
             {
                 var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=swiperankings;AccountKey=ReTB+/YWBrAeD7cC6//WrG2iRbG6D8ErOQRKI+Vcs5YJhXnQX/JFold6bsbW+Y5dFB9lGZUhoKpLat/o5b1gRA==");
+                //CloudConfigurationManager.GetSetting("swiperankings");
                 var client = account.CreateCloudBlobClient();
 
                 var rankings = client.GetContainerReference("rankings");
                 await rankings.CreateIfNotExistsAsync();
                 MD5 md5Hasher = MD5.Create();
                 var hash = md5Hasher.ComputeHash(this.Request.Body);
-                var blob = rankings.GetBlockBlobReference(param.list + "/" + BitConverter.ToString(hash).ToString().Replace("=",""));
-
-
+                var blob = rankings.GetBlockBlobReference(param.list + "/" + BitConverter.ToString(hash).ToString().Replace("-",""));
+                this.Request.Body.Seek(0, System.IO.SeekOrigin.Begin);
                 await blob.UploadFromStreamAsync(this.Request.Body);
                 return HttpStatusCode.OK;
             };

a3194f1980d57d5b6848130d5d5dc251a6663347
Paul Miller <paul.miller@gmail.com>
2015-10-08 21:50:14 -0700

    4 new lists including puppies and some scraper scripts

---
+++ helpers/scrapemissusa.ps1
@@ -0,0 +1,10 @@
+param($year)
+$(wget http://www.missuniverse.com/missusa/members/contestants/superregion:missusa/year:$year/category:GLA).content > contestants.txt
+$profiles = gc contestants.txt | select-string  "members/profile"  | % { $_.line } | ? { $_ -notmatch "Click" }  | % { $_.split("`"")[3] } 
+$profiles | % {
+  $r = Invoke-webrequest ("http://missuniverse.com" + $_);
+  $name = $r.AllElements | ? { $_.tagName -eq "title" }
+  $i= $r.Images | ? { $_.src -match "photographer" }
+  $img = $i[0]
+  @{ name=$name.innerHTML;img=$img.src }
+ } 

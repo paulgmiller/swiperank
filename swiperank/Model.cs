4d9bf509d65d49f0dccbff975b3f6fb054bbf717
Paul Miller <paul.miller@gmail.com>
2015-10-25 22:32:49 -0700

    missing model

---
+++ swiperank/Model.cs
@@ -0,0 +1,18 @@
+﻿namespace swiperank
+{
+    using System.Collections.Generic;
+
+    public class Ranking : List<Entry>
+    {
+        public string ListName;
+        public string Hash;
+        public string PrettyListName { get { return ListName.Replace("_", " "); } }
+    }
+
+    public class Entry
+    {
+        public string name;
+        public string img;
+        public string cachedImg;
+    }
+}

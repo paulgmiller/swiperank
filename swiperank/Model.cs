c32e7759ba7aa01f598138d18e60de3a6ceb1885
Paul Miller <paul.miller@gmail.com>
2016-06-04 14:37:16 -0700

    ed

--- swiperank/Model.cs
+++ swiperank/Model.cs
@@ -83,7 +83,7 @@
                     ranked.Add(rank.img, new EntryWithScore {
                         name = rank.name,
                         img = rank.img,
-                        cachedImg = rank.img, ///why?
+                        cachedImg = rank.cachedImg ?? rank.img;
                         score = score,
                     });
                 }
